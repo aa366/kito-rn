500e96bf82506918a11aa57aa48a84ec1d327de9
abdelwahab <bad3662067@gmail.com>
2026-08-17 19:11:57 +0300

    add: clerk

---
+++ src/app/_layout.tsx
@@ -0,0 +1,24 @@
+import { Stack } from 'expo-router'
+import {ClerkProvider} from "@clerk/expo"
+import { tokenCache } from '@clerk/expo/token-cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+
+const Layout = () => {
+    return (
+        <ClerkProvider  publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Stack>
+            <Stack.Screen name='index' options={{ headerShown: false }} />
+            <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+            <Stack.Screen name='(root)' options={{ headerShown: false }} />
+        </Stack>
+        </ClerkProvider>
+    )
+}
+
+export default Layout
--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,37 +1,38 @@
+import { Text, TouchableOpacity } from "react-native";
 
-import { ButtonProps } from "@/types/type"
-import { Text, TouchableOpacity } from 'react-native'
+import { ButtonProps } from "@/types/type";
 
 const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
     switch (variant) {
         case "secondary":
-            return "bg-gray-500"
+            return "bg-gray-500";
         case "danger":
-            return "bg-red-500"
+            return "bg-red-500";
         case "success":
-            return "bg-green-500"
+            return "bg-green-500";
         case "outline":
-            return "bg-transparent border-neutral-300 border-[0.5px]"
+            return "bg-transparent border-neutral-300 border-[0.5px]";
         default:
-            return "bg-[#0286FF]"
+            return "bg-[#0286FF]";
     }
-}
+};
 
 const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
     switch (variant) {
         case "primary":
-            return "text-black"
+            return "text-black";
         case "secondary":
-            return "text-gray-500"
+            return "text-gray-100";
         case "danger":
-            return "text-red-100"
+            return "text-red-100";
         case "success":
-            return "text-green-100"
+            return "text-green-100";
         default:
-            return "text-white"
+            return "text-white";
     }
-}
-export default function CustomButton({
+};
+
+const CustomButton = ({
     onPress,
     title,
     bgVariant = "primary",
@@ -40,16 +41,20 @@ export default function CustomButton({
     IconRight,
     className,
     ...props
-
-}: ButtonProps) {
+}: ButtonProps) => {
     return (
         <TouchableOpacity
             onPress={onPress}
             className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
-            {...props}>
+            {...props}
+        >
             {IconLeft && <IconLeft />}
-            <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>{title}</Text>
+            <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+                {title}
+            </Text>
             {IconRight && <IconRight />}
         </TouchableOpacity>
-    )
-}
+    );
+};
+
+export default CustomButton;
