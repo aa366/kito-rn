c76cd95f675e9cd400368fa0cdf3a04f7df73efe
abdelwahab <bad3662067@gmail.com>
2026-08-19 22:44:07 +0300

    add: not found page

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,10 +1,30 @@
-import { Text } from "react-native";
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function NotFound() {
   return (
-    <SafeAreaView>
-      <Text className="text-red-500 m-auto">Not Found page</Text>
-    </SafeAreaView>
+    <>
+      <Stack.Screen options={{ title: "Not Found" }} />
+      <SafeAreaView style={styles.container}>
+        <Text >Not Found page</Text>
+        <Text >This Screen doesn't exist.</Text>
+        <Link href={"/"} style={styles.link}>
+          Go to home screen!</Link>
+      </SafeAreaView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15
+  }
+})
